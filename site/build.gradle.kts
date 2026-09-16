9ce72e15a1ebcf45d36df2c47d64b2ba66746ffe
kodokenshi <89158701+kodokenshi@users.noreply.github.com>
2026-09-16 17:19:24 -0300

    build: aula 34.4 lock dependencies versions on `package.json`

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -18,6 +18,7 @@ buildscript {
 plugins {
   id("io.gitlab.arturbosch.detekt") version "1.23.8"
   id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+  id("io.github.ben-manes.versions") version "0.63.1"
   alias(libs.plugins.kotlin.multiplatform)
   alias(libs.plugins.compose.compiler)
   alias(libs.plugins.kobweb.application)
@@ -93,16 +94,14 @@ kotlin {
   sourceSets {
 		
     commonMain.dependencies {
-			
       implementation(kotlin("test"))
-      implementation("io.ktor:ktor-client-core:3.5.0")
+      implementation("io.ktor:ktor-client-core:3.6.0")
       implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
       implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
       implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
     }
 		
     jsMain.dependencies {
-			
       implementation(libs.compose.runtime)
       implementation(libs.compose.html.core)
       implementation(libs.kobweb.core)
@@ -111,15 +110,14 @@ kotlin {
       // Uncomment the following if you want access to a large set of font-awesome icons:
       // implementation(libs.silk.icons.fa)
       implementation(libs.kobwebx.markdown)
-      implementation("io.ktor:ktor-client-js:3.5.0")
+      implementation("io.ktor:ktor-client-js:3.6.0")
     }
 		
     jvmMain.dependencies {
-			
       implementation("org.junit.jupiter:junit-jupiter-api:6.1.3")
 			
       compileOnly(libs.kobweb.api) // Provided by Kobweb backend at runtime
-      implementation("io.ktor:ktor-client-cio:3.5.0")
+      implementation("io.ktor:ktor-client-cio:3.6.0")
 			
       implementation("org.jetbrains.exposed:exposed-core:1.5.0")
       implementation("org.jetbrains.exposed:exposed-dao:1.5.0")
@@ -134,7 +132,6 @@ kotlin {
 }
 
 tasks.withType<Test> {
-	
   useJUnitPlatform()
   testLogging {
 		
