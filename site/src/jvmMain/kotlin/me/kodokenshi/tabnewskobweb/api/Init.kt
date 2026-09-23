6d68abdd12238403abdddbde3798aa044cea7406
kodokenshi <89158701+kodokenshi@users.noreply.github.com>
2026-09-23 09:27:48 -0300

    refactor: update `/api/v1/Init` to use `defaultMigrations()` from `/database/migrations/Migrations`

--- site/src/jvmMain/kotlin/me/kodokenshi/tabnewskobweb/api/Init.kt
+++ site/src/jvmMain/kotlin/me/kodokenshi/tabnewskobweb/api/Init.kt
@@ -2,8 +2,7 @@ package me.kodokenshi.tabnewskobweb.api
 
 import com.varabyte.kobweb.api.init.InitApi
 import com.varabyte.kobweb.api.init.InitApiContext
-import me.kodokenshi.tabnewskobweb.database.Database
-import me.kodokenshi.tabnewskobweb.database.migrations.Migrations
+import me.kodokenshi.tabnewskobweb.database.migrations.defaultMigrations
 import me.kodokenshi.tabnewskobweb.infra.InternalServerError
 
 @InitApi
@@ -11,13 +10,7 @@ fun init(ctx: InitApiContext) {
   if (System.getenv("KOBWEB_BUILD_TYPE") == "prod") return
 	
   try {
-    Migrations(
-      driver = Database.POSTGRES_DRIVER,
-      databaseURL = Database.POSTGRES_URL,
-      databaseUser = Database.POSTGRES_USER,
-      databasePassword = Database.POSTGRES_PASSWORD,
-      migrationsPath = "infra/migrations",
-    ).migrate()
+    defaultMigrations().migrate()
   } catch (e: Throwable) {
     ctx.logger.error(InternalServerError(cause = e).toString())
   }
