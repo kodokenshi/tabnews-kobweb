9c0bfe1b9a0671d6614161561ba103ae901e321c
kodokenshi <89158701+kodokenshi@users.noreply.github.com>
2026-09-23 11:48:40 -0300

    ci: add `create-users` migration sql

---
+++ site/infra/migrations/1790167957884_kodokenshi_create-users.sql
@@ -0,0 +1,9 @@
+create table users
+(
+    id         uuid primary key default gen_random_uuid(),
+    username   varchar(30)  not null unique,
+    email      varchar(254) not null unique,
+    passwd     varchar(72)  not null,
+    created_at timestamptz      default now(),
+    updated_at timestamptz      default now()
+)
